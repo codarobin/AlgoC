7126420cb16a77a82bc864c74173f0ce17a0d3b0
codarobin <robin.coda@cpe.fr>
2020-10-24 12:34:37 +0200

    Exercice 1.3

--- TP/TP1/src/sizeof_types.c
+++ TP/TP1/src/sizeof_types.c
@@ -2,14 +2,29 @@
 
 #include <stdio.h>
 
-int main() {
+int main(){
 
-  printf("char %ld\n", sizeof(char));
-  printf("short %ld\n", sizeof(short));
-  printf("int %ld\n", sizeof(int));
-  printf("long int %ld\n", sizeof(long int));
-  printf("long long int %ld\n", sizeof(long long int));
-  printf("float %ld\n", sizeof(float));
-  printf("double %ld\n", sizeof(double));
-  return (0);
+    printf("Char : %ld octets\n", sizeof(char));
+    printf("Unsigned Char : %ld octets\n", sizeof(unsigned char));
+    
+    printf("Short : %ld octets\n", sizeof(short));
+    printf("Unsigned Short : %ld octets\n", sizeof(unsigned short));
+    
+    printf("Int : %ld octets\n", sizeof(int));
+    printf("Unsigned Int : %ld octets\n", sizeof(unsigned int));
+    
+    printf("Long Int : %ld octets\n", sizeof(long int));
+    printf("Unsigned Long Int : %ld octets\n", sizeof(unsigned long int));
+    
+    printf("Long Long Int : %ld octets\n", sizeof(long long int));
+    printf("Unsigned Long Long Int : %ld octets\n", sizeof(unsigned long long int));
+    
+    printf("Float : %ld octets\n", sizeof(float));
+    
+    printf("Double : %ld octets\n", sizeof(double));
+    
+    printf("Long Double : %ld octets\n", sizeof(long double));
+
+    return 0;
 }
+
