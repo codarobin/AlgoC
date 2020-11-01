0706fa9c1464afbbbf8006968808366917310493
codarobin <robin.coda@cpe.fr>
2020-11-01 16:55:15 +0100

    test

--- TP/TP1/src/tempCodeRunnerFile.c
+++ TP/TP1/src/tempCodeRunnerFile.c
@@ -1,37 +1,7 @@
-int main(){
-    
-    int a = 16;
-    int b = 3;
-
-    printf("a + b = %d\n", a + b);
-    printf("a - b = %d\n", a - b);
-    printf("a * b = %d\n", a * b);
-    printf("a / b = %d\n", a / b);
-    printf("a %% b = %d\n", a % b);
-
-    if (a > b){
-        printf("a > b\n");
-    }
-
-    if (a >= b){
-        printf("a >= b\n");
-    }
-
-
-    if (b < a){
-        printf("b < a\n");
-    }
-
-
-    if (b <= a){
-        printf("b <= a\n");
-    }
-
-
-    if (a != b){
-        printf("a != b\n");
-    }
-
-
-    return 0;
-}
+for (int r = compter; r >= (compter - l); r--){
+            if (((r + 1) > compter) || (r <= (compter-l))) {
+                printf("* ");
+            } else {
+                printf("# ");
+            }
+        }
