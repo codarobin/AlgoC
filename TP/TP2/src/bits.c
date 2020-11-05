b50b403da7cdb4bef0fcdb233fc83eec71f180aa
codarobin <robin.coda@cpe.fr>
2020-11-05 11:40:08 +0100

    2.2

--- TP/TP2/src/bits.c
+++ TP/TP2/src/bits.c
@@ -1,27 +1,16 @@
 #include <stdio.h>  
 int main(){
   
-  int i;
-  int max;
-  int tab[i];
-  int d = 192484616; 
-  for(i=0; d > 0; i++)  
-  {  
-    tab[i] = d%2;  
-    d = d/2;
-    max = i;  
-  } 
-  for(i = max; i != 0; i--)  
-  {  
-    printf("%d",tab[i]); //chaine de caractère   
-  }
-  if((tab[max - 3] ) && (tab[max - 19]))
+  int mask = 268439552;
+  int d = 268439552;
+  if (d & mask)
   {
-      printf("\n1");
+    printf("ok");
   }
   else
   {
-      printf("\n0");
+    printf("nok");
   }
+  
   return 0;
 }
